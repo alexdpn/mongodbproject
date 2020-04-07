6919bdef9f03ece0a10700c4a1fee51ab4d7af2d
alex <alexandruponea45@gmail.com>
2020-04-07 19:16:53 +0300

    Integrated logger in RestApp class

--- src/main/java/com/project/mongodb/RestApp.java
+++ src/main/java/com/project/mongodb/RestApp.java
@@ -6,14 +6,21 @@ import javax.ws.rs.core.UriBuilder;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.URI;
 
 public class RestApp {
+
+    private static final Logger logger = LoggerFactory.getLogger(RestApp.class);
+
     public static void main(String[] args) {
         //configure the embedded jdk server
         URI uri = UriBuilder.fromUri("http://localhost/").port(4000).build();
         ResourceConfig resourceConfig = new ResourceConfig(CompanyController.class);
+
+        logger.info("Starting embedded http server on port {}", uri.getPort());
         HttpServer httpServer = JdkHttpServerFactory.createHttpServer(uri, resourceConfig);
     }
 }
