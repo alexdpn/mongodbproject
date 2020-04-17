d45db641d75e1f4e772d5a88774828b80b74da98
alex <alexandruponea45@gmail.com>
2020-04-17 15:15:10 +0300

    Refactorization

--- src/main/java/com/project/mongodb/util/DataUtil.java
+++ src/main/java/com/project/mongodb/util/DataUtil.java
@@ -4,23 +4,21 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.mongodb.model.Company;
 
-import java.io.File;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class DataUtil {
-    private static final String RELATIVE_PATH = "/src/main/resources/companies.json";
-    private static final String BASE_PATH = System.getProperty("user.dir");
+    private static final String PATH = "/META-INF/companies.json";
 
     public static List<Company> getCompaniesFromJsonFile() {
         List<Company> list = new ArrayList<>(5);
-        File file = new File(BASE_PATH + RELATIVE_PATH);
 
         try {
-            ObjectMapper objectMapper = new ObjectMapper();
+            InputStream stream = DataUtil.class.getResourceAsStream(PATH);
 
-            list = objectMapper.readValue(file, new TypeReference<List<Company>>() {});
+            ObjectMapper objectMapper = new ObjectMapper();
+            list = objectMapper.readValue(stream, new TypeReference<List<Company>>() {});
 
             return list;
         }catch(Exception e ) {
