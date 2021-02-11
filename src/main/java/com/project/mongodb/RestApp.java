ac4fce4ac0a551670271f7010411fc4f5dda9011
alex <alexandruponea45@gmail.com>
2021-02-11 23:25:12 +0200

    Added Lombok on RestApp class

--- src/main/java/com/project/mongodb/RestApp.java
+++ src/main/java/com/project/mongodb/RestApp.java
@@ -6,6 +6,7 @@ import com.project.mongodb.helper.EmbeddedMongoDbHelper;
 
 import javax.ws.rs.core.UriBuilder;
 
+import lombok.extern.slf4j.Slf4j;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.linking.DeclarativeLinkingFeature;
@@ -17,22 +18,21 @@ import org.glassfish.grizzly.http.server.HttpServer;
 
 import java.net.URI;
 
+@Slf4j
 public class RestApp {
 
-    private static final Logger logger = LoggerFactory.getLogger(RestApp.class);
-
     public static void main(String[] args) {
-        logger.info("Configuring Jersey");
+        log.info("Configuring Jersey");
         URI uri = UriBuilder.fromUri("http://localhost/").port(4000).build();
         ResourceConfig resourceConfig = new ResourceConfig(CompanyController.class);
         resourceConfig.register(new CompanyBinder());
         resourceConfig.register(DeclarativeLinkingFeature.class);
         resourceConfig.register(JacksonFeature.class);
 
-        logger.info("Starting embedded database");
+        log.info("Starting embedded database");
         EmbeddedMongoDbHelper.startDatabase();
 
-        logger.info("Starting embedded http server on port {}", uri.getPort());
+        log.info("Starting embedded http server on port {}", uri.getPort());
         HttpServer httpServer = GrizzlyHttpServerFactory.createHttpServer(uri, resourceConfig);
     }
 }
