465466a023fec9361039ce3c870f3966af0b9e34
alex <alexandruponea45@gmail.com>
2021-02-17 23:35:22 +0200

    Fixed typo

--- src/main/java/com/project/mongodb/security/SecurityProperties.java
+++ src/main/java/com/project/mongodb/security/SecurityProperties.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class SecurityProperties {
-    //the passwords should be put in environment variables but we'll keep it simple
+    //the password should be put in environment variables but we'll keep it simple
     private String keystorePath = "src/main/resources/META-INF/security/keystore.jks";
     private String keyStorePassword = "pass123";
 }
