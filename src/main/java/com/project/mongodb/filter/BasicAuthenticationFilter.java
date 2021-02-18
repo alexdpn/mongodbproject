dca6d98ba74a78f84db97aea880f874a5fd23080
alex <alexandruponea45@gmail.com>
2021-02-18 20:09:21 +0200

    Implemented BasicAuthenticationFilter

---
+++ src/main/java/com/project/mongodb/filter/BasicAuthenticationFilter.java
@@ -0,0 +1,27 @@
+package com.project.mongodb.filter;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.util.Base64;
+
+public class BasicAuthenticationFilter implements ContainerRequestFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        if(!checkBasicAuthCredentials(requestContext)) {
+            requestContext.abortWith(Response
+                    .status(Response.Status.UNAUTHORIZED)
+                    .header("WWW-Authenticate", "Basic realm=\"User Visible Realm\", charset=\"UTF-8\"")
+                    .build());
+        }
+    }
+
+    private boolean checkBasicAuthCredentials(ContainerRequestContext request) {
+        String hashedCredentials = "Basic " + Base64.getEncoder().encodeToString("jersey:pass12".getBytes());
+        String authorizationHeader = request.getHeaderString("Authorization");
+
+        return authorizationHeader != null && hashedCredentials.equals(authorizationHeader);
+    }
+}
