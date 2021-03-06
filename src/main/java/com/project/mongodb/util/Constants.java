7ff13ad4604f76607c9ab9cba49440593f6d53bb
alex <alexandruponea45@gmail.com>
2021-03-06 08:23:35 +0200

    Created Constants class

---
+++ src/main/java/com/project/mongodb/util/Constants.java
@@ -0,0 +1,11 @@
+package com.project.mongodb.util;
+
+public class Constants {
+    public static final String SELF = "self";
+    public static final String DATABASE = "pojodb";
+    public static final String COLLECTION = "companies";
+    public static final String CONNECTION_STRING = "mongodb://localhost:27017";
+    public static final String ID = "_id";
+    public static final String PATH = "/META-INF/companies.json";
+    public static final String HOST = "localhost";
+}
